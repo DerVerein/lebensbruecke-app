338670a844bf2c81a4b29a275e0af4267c9cdf1c
DerVerein <74hexe@online.de>
2026-09-15 07:35:22 +0200

    Add service worker for caching assets

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = "lebensbruecke-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./VereinsIcon.png",
+  "./hilfe.html",
+  "./mensch.html",
+  "./spenden.html",
+  "./finanzielle-notlage.html"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
